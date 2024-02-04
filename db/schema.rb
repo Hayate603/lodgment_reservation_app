5ec73721ca8c3e752a40b4d6422fa97883ac106e
Hayate Fukashiro <hayatefukashiro@mb.local>
2024-02-05 06:21:22 +0900

    Roomモデルにprefectureカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_04_185806) do
+ActiveRecord::Schema.define(version: 2024_02_04_211915) do
 
   create_table "reservations", force: :cascade do |t|
     t.date "check_in"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2024_02_04_185806) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+    t.string "prefecture"
     t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
